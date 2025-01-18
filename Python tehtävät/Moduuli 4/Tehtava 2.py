651a251477947efab0602824d09e5fe35cf358a7
Jekoth <Rizvan8103@gmail.com>
2025-01-18 18:14:48 +0200

    Fixed code. Now it end only after negative value

--- Python tehtävät/Moduuli 4/Tehtava 2.py
+++ Python tehtävät/Moduuli 4/Tehtava 2.py
@@ -1,10 +1,8 @@
 convert = float(input("Enter number in inches: "))
 
-while convert > 0:  #if positive
+while convert >= 0:  #if positive
     cm = convert * 2.54  #Inches to cm formula
     print(f"{convert} inches is {cm} centimeters")
-    break
 
-#if not positive
-if convert <= 0:
-    print("Please enter a positive number.")
+    convert = float(input("Enter number in inches: "))
+
