47ca60a57b0538285c0ec913390c60d9b2fcc1d5
Jekoth <Rizvan8103@gmail.com>
2025-01-18 16:51:08 +0200

    Made some corrections in if statement. added invalid gender function.

--- Python tehtävät/Moduuli 3/Tehtava 3.py
+++ Python tehtävät/Moduuli 3/Tehtava 3.py
@@ -1,17 +1,20 @@
 gender = input("Enter biological gender (male/female): ")
-hemoglobin_value = float(input("Enter your hemoglobin value"))
+hemoglobin_value = float(input("Enter your hemoglobin value: "))
 
 #Normal hemoglobin level
 if gender == "female":
     normal_range = (117, 155)
-else: #gender == "male":s
+elif gender == "male":
     normal_range = (134, 167)
-
-
-#Checks if the hemoglobin is low, normal or high
-if(hemoglobin_value == normal_range[0] ):
-    print("Your hemoglobin value is low ")
-elif(hemoglobin_value == normal_range[1]):
-    print("Your hemoglobin value is normal ")
 else:
-    print("Your hemoglobin value is high")
+    print("Invalid gender entered.")
+    exit()
+
+
+# Check if the hemoglobin is low, normal, or high
+if hemoglobin_value < normal_range[0]:
+    print("Your hemoglobin value is low.")
+elif normal_range[0] <= hemoglobin_value <= normal_range[1]:
+    print("Your hemoglobin value is normal.")
+else:
+    print("Your hemoglobin value is high.")
