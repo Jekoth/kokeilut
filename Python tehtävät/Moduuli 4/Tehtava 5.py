f3e2dc6658c2a14a03d058af9ae9957fdb8447da
Jekoth <Rizvan8103@gmail.com>
2025-01-19 17:15:51 +0200

    Fixed code made it more clear and now everything works

--- Python tehtävät/Moduuli 4/Tehtava 5.py
+++ Python tehtävät/Moduuli 4/Tehtava 5.py
@@ -5,13 +5,16 @@ tries = 1
 user_name = input("Enter username: ")
 user_pass = input("Enter password: ")
 
-while user_name and user_pass != username and password or tries <= 5:
+while (user_name != username or user_pass != password) and tries <= 5: #Loop is true until correct credentials are entered
     if user_name == username and user_pass == password:
-        print("Welcome")
+        print("Welcome!")
+        break
+    if tries >= 5:
+        print("Access denied")
         break
     else:
-        print("Access denied")
-        tries = tries + 1
+        print("Incorrect, try again: ")
+        tries += 1
 
     user_name = input("Enter username: ")
     user_pass = input("Enter password: ")
