883cb2d15c791817e5db7becfeeb091346f5dc7b
Jekoth <Rizvan8103@gmail.com>
2025-01-25 16:31:55 +0200

    Added numbers to the empty list then reversed,sorted and split them to get top five number.

--- Python tehtävät/Moduuli 5/Tehtava 2.py
+++ Python tehtävät/Moduuli 5/Tehtava 2.py
@@ -1,12 +1,21 @@
 num = []
 
-print("Enter Number. Press enter to stop program")
+print("Press enter to stop program")
 
 while True:
     user_num = input("Enter a number: ")
     if user_num == "": #Stop program if user enter empty string.
         break
     try:
-        number = float(user_num) #Conv string to float
+        number = int(user_num) #Conv string to int
+        num.append(number)
     except ValueError:
-        print("Invalid input")
+        print("Invalid input") #number is added to the empty list
+
+num.sort(reverse=True)
+
+top_num = num[:5] #Splitting reverse sorted list
+
+print("\nThe five top number are: ")
+for number in top_num:
+    print(number)
