46c40d4ade3d9eb169824be4d1139ed1f1655741
Jekoth <80098768+Jekoth@users.noreply.github.com>
2025-01-31 19:20:22 +0200

    Added function that calculates radius, area, square meters and unit price and then returns each square meter price

--- Python tehtävät/Moduuli 6/Tehtava 6.py
+++ Python tehtävät/Moduuli 6/Tehtava 6.py
@@ -1,6 +1,10 @@
 #func that calculates diameter and price of pizza per square meter
+import math
 
-#radius
-#area
-#square meters
-#unit price
+def calculate_diameter_price(diameter, price):
+    radius = diameter / 2   #Calculates radius
+    area = math.pi * (radius * 2)    #Calculates circle area of the pizza with pi
+    square_meters = area / 10000    #Calculates square meters by converting cm2 from area to m2
+    unit_price = price / square_meters    # calculates unit price per square meter
+    return unit_price
+
