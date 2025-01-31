4fb68b29fd38b8402c6fd593525233b9a07243fe
Jekoth <80098768+Jekoth@users.noreply.github.com>
2025-01-31 19:34:40 +0200

    Added function to compare pizza prices for better value

--- Python tehtävät/Moduuli 6/Tehtava 6.py
+++ Python tehtävät/Moduuli 6/Tehtava 6.py
@@ -8,7 +8,21 @@ def calculate_diameter_price(diameter, price):
     unit_price = price / square_meters    # calculates unit price per square meter
     return unit_price
 
-#main
+def main():
+    #get first and second pizza input
+    diameter1 = int(input("Enter the diameter of the first pizza in cm: "))
+    price1 = int(input("Enter the price of the first pizza in euros: "))
 
-#get first and second pizza input
-# compares better value per square meter
+    diameter2 = int(input("Enter the diameter of the first pizza in cm: "))
+    price2 = int(input("Enter the price of the first pizza in euros: "))
+
+
+    # compares better value per square meter
+    if price1 < price2:
+        print("First pizza is better value")
+    elif price1 > price2:
+        print("Second pizza is better value")
+    else:
+        print("Both pizzas have same value")
+
+main()
