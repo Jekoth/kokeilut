23a847feeff0255bd1c7c0e37d46cd2569a5a4df
Jekoth <80098768+Jekoth@users.noreply.github.com>
2025-01-31 18:52:36 +0200

    Added remove_uneven_numbers function that checks even numbers from even list and main function that removes even numbers from original list
    fixed wrong code

--- Python tehtävät/Moduuli 6/Tehtava 5.py
+++ Python tehtävät/Moduuli 6/Tehtava 5.py
@@ -1,4 +1,4 @@
-#function that return even and uneven number from the list of integers
+#function that return original and even number from the list of integers
 
 def remove_uneven_numbers(input_list):
     even_numbers = [] #even list
@@ -11,9 +11,9 @@ def remove_uneven_numbers(input_list):
     return even_numbers#Return list of even numbers
 
 def main():
-    original_list = [1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9 , 10] # even list
+    original_list = [1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9 , 10] # original list
 
-    even_list = remove_uneven_numbers(original_list)
+    even_list = remove_uneven_numbers(original_list) #removes even numbers from original list
 
     print("Original list: ", original_list)
     print("Even list: ", even_list)
