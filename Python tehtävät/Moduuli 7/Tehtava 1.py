004e28740c264981ccd52008991bfd4283d24020
Jekoth <80098768+Jekoth@users.noreply.github.com>
2025-02-04 07:06:37 +0200

    Added better error handling and made code cleaner

--- Python tehtävät/Moduuli 7/Tehtava 1.py
+++ Python tehtävät/Moduuli 7/Tehtava 1.py
@@ -1,9 +1,15 @@
 vuodenajat = ("talvi", "talvi", "kevät", "kevät", "kevät", "kesä", "kesä", "kesä", "syksy", "syksy", "syksy", "talvi")
 
-kuukaudennumero = int(input("Anna kuukauden järjestysnumero (1-12): "))
 
-if 1 <= kuukaudennumero <= 12:
-        vuodenaika = vuodenajat[kuukaudennumero-1]
-        print (f"Kuukausi {kuukaudennumero} on {vuodenaika}.")
-else:
-    print("Virheellinen numero! Anna luku 1-12")
+while True:
+    try:
+        kuukaudennumero = int(input("Anna kuukauden järjestysnumero (1-12): "))
+
+        if 1 <= kuukaudennumero <= 12:
+                vuodenaika = vuodenajat[kuukaudennumero-1]
+                print (f"Kuukausi {kuukaudennumero} on {vuodenaika}.")
+                break
+        else:
+            print("Virheellinen numero! Anna luku 1-12")
+    except ValueError:
+        print("VIRHE! Syötä numero 1-12")
