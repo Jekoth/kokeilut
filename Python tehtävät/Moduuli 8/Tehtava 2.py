4357d313704971a1e6be96bfb6498d03f9ec921e
Jekoth <80098768+Jekoth@users.noreply.github.com>
2025-02-07 10:26:33 +0200

    Added

---
+++ Python tehtävät/Moduuli 8/Tehtava 2.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='password',
+    autocommit=True,
+    collation='utf8mb3_unicode_ci'
+)
+
+country_code = input("Enter the country code(FI, USA): ")
+
+#SQL
+toimi = yhteys.cursor()
+toimi.execute("""
+    SELECT type, COUNT(*) FROM airport WHERE iso_country = %s GROUP BY type """, (country_code,))
+
+tulokset = toimi.fetchall()
+
+toimi.close()
+yhteys.close()
+
+#Searches for database and prints data
+if tulokset:
+    print(f"Airports in country {country_code} by type:")
+    for airport_type, count in tulokset:
+        print(f"{airport_type}: {count}")
+else:
+    print("No airports found for the given country code.")
