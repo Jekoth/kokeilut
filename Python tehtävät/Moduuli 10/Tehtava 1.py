3584d3d9e03a173c06857a2bf34f05ad79478938
Jekoth <80098768+Jekoth@users.noreply.github.com>
2025-03-20 22:02:11 +0200

    added funktion to go up and down and error handling for out of range request

--- Python tehtävät/Moduuli 10/Tehtava 1.py
+++ Python tehtävät/Moduuli 10/Tehtava 1.py
@@ -9,9 +9,22 @@ class Elevator:
             print(f"Floor is out of range!")
             return
 
+        while self.cur_floor < target_floor:
+            self.floor_up()
+
+        while self.cur_floor > target_floor:
+            self.floor_down()
+
     def floor_up(self):
-        pass
+        if self.cur_floor < self.top_floor:
+            self.cur_floor += 1
+            print(f"Going up: Current floor number is {self.cur_floor}")
+        else:
+            print("Already at the top floor!")
 
     def floor_down(self):
-        pass
-
+        if self.cur_floor > self.bottom_floor:
+            self.cur_floor -= 1
+            print(f"Going down: Current floor number is {self.cur_floor}")
+        else:
+            print("Already at the bottom floor!")
